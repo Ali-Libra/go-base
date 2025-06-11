b6e80ec27c706a3876757a835efc7d19d13e9dc0
ali <chenli@foxmail.com>
2025-06-11 18:05:57 +0800

    优化tcpserver逻辑

--- net/tcp/tcp_server.go
+++ net/tcp/tcp_server.go
@@ -5,6 +5,7 @@ import (
 	"go-base/logger"
 	"net"
 	"sync"
+	"time"
 )
 
 type RecvMessage struct {
@@ -21,11 +22,13 @@ type TcpServer struct {
 	port    string
 	IdCount uint64
 
-	rwLock    sync.RWMutex
-	conns     map[uint64]net.Conn
-	recvChan  chan *RecvMessage // 用于接收消息的通道
-	sendChan  chan *SendMessage // 用于发送消息的通道
-	closeChan chan uint64       // 用于关闭连接的通道
+	rwLock     sync.RWMutex
+	conns      map[uint64]net.Conn
+	recvChan   chan *RecvMessage // 用于接收消息的通道
+	sendChan   chan *SendMessage // 用于发送消息的通道
+	closeChan  chan uint64       // 用于关闭连接的通道
+	closeRead  chan struct{}
+	closeWrite chan struct{}
 
 	onConnect func(conn uint64)
 	onMessage func(conn uint64, msg []byte)
@@ -34,11 +37,13 @@ type TcpServer struct {
 
 func NewTcpServer(port string) *TcpServer {
 	return &TcpServer{
-		port:      port,
-		recvChan:  make(chan *RecvMessage, 10240), // 初始化消息通道
-		sendChan:  make(chan *SendMessage, 10240), // 初始化发送通道映射
-		closeChan: make(chan uint64),              // 初始化关闭通道
-		IdCount:   0,
+		port:       port,
+		recvChan:   make(chan *RecvMessage, 10240), // 初始化消息通道
+		sendChan:   make(chan *SendMessage, 10240), // 初始化发送通道映射
+		closeChan:  make(chan uint64),              // 初始化关闭通道
+		closeRead:  make(chan struct{}),
+		closeWrite: make(chan struct{}),
+		IdCount:    0,
 	}
 }
 
@@ -98,15 +103,18 @@ func (s *TcpServer) OnLoop() {
 }
 
 func (s *TcpServer) Close() {
-	s.rwLock.Lock()
-	for connId, conn := range s.conns {
-		conn.Close()
-		delete(s.conns, connId)
-	}
-	s.rwLock.Unlock()
+	//先关闭接受通道，不再接收客户端消息
+	s.closeRead <- struct{}{}
 
-	close(s.recvChan)
-	close(s.sendChan)
+	for {
+		if len(s.sendChan) == 0 {
+			s.closeWrite <- struct{}{}
+			for connId, conn := range s.conns {
+				conn.Close()
+				delete(s.conns, connId)
+			}
+		}
+	}
 }
 
 func (s *TcpServer) SendMessage(connId uint64, data []byte) {
@@ -134,72 +142,92 @@ func (s *TcpServer) handleConn(conn net.Conn) {
 	var readData []byte
 	var readDataTotal uint32 = 0
 	for {
-		if readLenTotal < 4 {
-			n, err := conn.Read(readLenBuf[readLenTotal:]) // 读取长度前缀
-			if err != nil {
-				logger.Error("connect %d read msg  error %v", connId, err)
-				return // 读取失败，退出循环
+		select {
+		case <-s.closeRead:
+			close(s.recvChan)
+		default:
+			if readLenTotal < 4 {
+				deadline := time.Now().Add(10 * time.Millisecond)
+				conn.SetReadDeadline(deadline)
+				n, err := conn.Read(readLenBuf[readLenTotal:]) // 读取长度前缀
+				if err != nil {
+					if ne, ok := err.(net.Error); ok && !ne.Timeout() {
+						logger.Error("connect %d read msg  error %v", connId, err)
+						return // 读取失败，退出循环
+					}
+				}
+				readLenTotal += uint32(n)
 			}
-			readLenTotal += uint32(n)
-		}
-		if readLenTotal < 4 {
-			continue
-		}
-
-		length := binary.BigEndian.Uint32(readLenBuf)
-		if readDataTotal == 0 {
-			if length == 0 || length > 10*1024*1024 {
-				logger.Error("connect %d read msg len error", connId)
-				return // 长度不合法，退出循环
+			if readLenTotal < 4 {
+				continue
 			}
-			readData = make([]byte, length) // 分配足够的空间来存储数据
-		}
-		if readDataTotal < length {
-			n, err := conn.Read(readData[readDataTotal:]) // 读取实际数据
-			if err != nil {
-				logger.Error("connect %d read msg  error %v", connId, err)
-				return // 读取失败，退出循环
+
+			length := binary.BigEndian.Uint32(readLenBuf)
+			if readDataTotal == 0 {
+				if length == 0 || length > 10*1024*1024 {
+					logger.Error("connect %d read msg len error", connId)
+					return // 长度不合法，退出循环
+				}
+				readData = make([]byte, length) // 分配足够的空间来存储数据
 			}
-			readDataTotal += uint32(n)
-		}
-		if readDataTotal < length {
-			continue
-		}
+			if readDataTotal < length {
+				deadline := time.Now().Add(10 * time.Millisecond)
+				conn.SetReadDeadline(deadline)
+				n, err := conn.Read(readLenBuf[readDataTotal:]) // 读取长度前缀
+				if err != nil {
+					if ne, ok := err.(net.Error); ok && !ne.Timeout() {
+						logger.Error("connect %d read msg  error %v", connId, err)
+						return // 读取失败，退出循环
+					}
+				}
+				readDataTotal += uint32(n)
+			}
+			if readDataTotal < length {
+				continue
+			}
 
-		readLenTotal = 0
-		readDataTotal = 0
+			readLenTotal = 0
+			readDataTotal = 0
 
-		// 4. 处理消息
-		s.recvChan <- &RecvMessage{
-			ConnId: connId, // 将连接和消息数据封装到 ConnMessage 中
-			Data:   readData,
+			// 4. 处理消息
+			s.recvChan <- &RecvMessage{
+				ConnId: connId, // 将连接和消息数据封装到 ConnMessage 中
+				Data:   readData,
+			}
 		}
 	}
 }
 
 func (s *TcpServer) handleWrite() {
-	for msg := range s.sendChan { // 从发送通道读取数据
-		length := uint32(len(msg.Data))
-		lenBuf := make([]byte, 4)
-		binary.BigEndian.PutUint32(lenBuf, length)
+	for {
+		select {
+		case <-s.closeWrite:
+			return
+		default:
+			for msg := range s.sendChan { // 从发送通道读取数据
+				length := uint32(len(msg.Data))
+				lenBuf := make([]byte, 4)
+				binary.BigEndian.PutUint32(lenBuf, length)
 
-		s.rwLock.RLock()
-		conn, ok := s.conns[msg.ConnId]
-		s.rwLock.RUnlock()
-		if !ok {
-			logger.Error("connect %d have  closed", msg.ConnId)
-			continue
-		}
+				s.rwLock.RLock()
+				conn, ok := s.conns[msg.ConnId]
+				s.rwLock.RUnlock()
+				if !ok {
+					logger.Error("connect %d have  closed", msg.ConnId)
+					continue
+				}
 
-		_, err := conn.Write(lenBuf) // 发送长度前缀
-		if err != nil {
-			logger.Error("connect %d have  write error", msg.ConnId)
-			continue
-		}
-		_, err = conn.Write(msg.Data)
-		if err != nil {
-			logger.Error("connect %d have  write error", msg.ConnId)
-			continue
+				_, err := conn.Write(lenBuf) // 发送长度前缀
+				if err != nil {
+					logger.Error("connect %d have  write error", msg.ConnId)
+					continue
+				}
+				_, err = conn.Write(msg.Data)
+				if err != nil {
+					logger.Error("connect %d have  write error", msg.ConnId)
+					continue
+				}
+			}
 		}
 	}
 }
