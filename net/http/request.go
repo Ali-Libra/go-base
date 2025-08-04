98b3d5e9753eb5ef6104528b278cf3d16629b8c2
ali <chenli201709@foxmail.com>
2025-08-04 19:03:08 +0800

    上下文支持int

--- net/http/request.go
+++ net/http/request.go
@@ -3,11 +3,12 @@ package http
 import (
 	"io"
 	"net/http"
+	"strconv"
 )
 
 type HttpRequest struct {
 	*http.Request
-	contexts map[string]string
+	contexts map[string]interface{}
 }
 
 func (req *HttpRequest) ReadBody() ([]byte, error) {
@@ -18,16 +19,53 @@ func (req *HttpRequest) ReadBody() ([]byte, error) {
 	return body, nil
 }
 
-func (req *HttpRequest) SetContext(key string, value string) {
+func (req *HttpRequest) SetContext(key string, value interface{}) {
 	if req.contexts == nil {
-		req.contexts = make(map[string]string)
+		req.contexts = make(map[string]interface{})
 	}
 	req.contexts[key] = value
 }
 
-func (req *HttpRequest) GetContext(key string) string {
+func (req *HttpRequest) GetContext(key string) interface{} {
 	if req.contexts == nil {
-		return ""
+		return nil
 	}
 	return req.contexts[key]
+}
+
+func (req *HttpRequest) GetContextString(key string) string {
+	if val, ok := req.contexts[key]; ok {
+		if s, ok := val.(string); ok {
+			return s
+		}
+	}
+	return ""
+}
+
+func (req *HttpRequest) GetContextUint64(key string) uint64 {
+	if val, ok := req.contexts[key]; ok {
+		switch v := val.(type) {
+		case uint64:
+			return v
+		case string:
+			if parsed, err := strconv.ParseUint(v, 10, 64); err == nil {
+				return parsed
+			}
+		}
+	}
+	return 0
+}
+
+func (req *HttpRequest) GetContextUint32(key string) uint32 {
+	if val, ok := req.contexts[key]; ok {
+		switch v := val.(type) {
+		case uint32:
+			return v
+		case string:
+			if parsed, err := strconv.ParseUint(v, 10, 32); err == nil {
+				return uint32(parsed)
+			}
+		}
+	}
+	return 0
 }
